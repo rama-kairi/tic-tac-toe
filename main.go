cfb4da26cec93e3a57cfcf881b23daf04032c5cf
Ramananda Kairi <ramanandakairi@gmail.com>
2022-12-29 22:35:29 +0530

    Game is in Progress

--- main.go
+++ main.go
@@ -3,44 +3,134 @@ package main
 import "fmt"
 
 var (
-	// Create a Data Structure to hold the board
-	// X - X - O
-	// O - X - X
-	// X - O - O
-	board [3][3]int
-	x     int = 1
-	o     int = 2
+	player1    = 1
+	player2    = 2
+	emptySpace = 0
 )
 
-func main() {
-	fmt.Println("Tic Tac Toe Game")
-	newBoard()
-	printBoard()
-	fmt.Printf("Board: %v", board)
-}
+var board [3][3]int
 
-// newBoard() - Initialize the board
+// newBoard -  creates a new board
 func newBoard() {
 	for i := 0; i < 3; i++ {
 		for j := 0; j < 3; j++ {
-			board[i][j] = 0
+			board[i][j] = emptySpace
 		}
 	}
 }
 
-// printBoard() - Print the board
+// printBoard - prints the board
 func printBoard() {
 	for i := 0; i < 3; i++ {
 		for j := 0; j < 3; j++ {
-			fmt.Printf("%v | ", board[i][j])
+			if board[i][j] == 2 {
+				fmt.Printf(" X |")
+			} else if board[i][j] == 1 {
+				fmt.Printf(" O |")
+			} else {
+				fmt.Printf("   |")
+			}
 		}
 		fmt.Println()
-		fmt.Println("-----------")
+		fmt.Println("----------")
 	}
 }
 
-// A function to take input from the user and update the board
+func main() {
+	newBoard()
+	fmt.Println("Welcome to Tic Tac Toe")
 
-// Check if the board is full, Meaning no more moves can be made and its draw
+	currentPLayer := 1
+	for {
+		fmt.Println("Current Board :")
+		printBoard()
 
-// Check if the game is won by either player
+		fmt.Printf("Hello Player %d, Please enter your move in row and column format (e.g. 1 2): ", currentPLayer)
+		var row, col int
+		_, err := fmt.Scanf("%d %d", &row, &col)
+		if err != nil {
+			fmt.Println("Invalid input, please try again")
+			continue
+		}
+
+		if row < 1 || row > 3 || col < 1 || col > 3 {
+			fmt.Println("Invalid input, please try again")
+			continue
+		}
+
+		updateBoard(row, col, currentPLayer)
+
+		// Check if someone won the game
+		isWon := checkWon()
+		if isWon {
+			updateBoard(row, col, currentPLayer)
+			printBoard()
+			break
+		}
+
+		// Check if the game is draw
+		isDraw := checkDraw()
+		if isDraw {
+			updateBoard(row, col, currentPLayer)
+			printBoard()
+			fmt.Println("Game is draw")
+			break
+		}
+
+		// Alternate the player
+		if currentPLayer == 1 {
+			currentPLayer = 2
+		} else {
+			currentPLayer = 1
+		}
+	}
+}
+
+// updateBoard - updates the board with the move
+func updateBoard(row, col, player int) {
+	board[row-1][col-1] = player
+}
+
+func checkWon() (isWon bool) {
+	// Check row
+	for i := 0; i < 3; i++ {
+		if board[i][0] == board[i][1] &&
+			board[i][1] == board[i][2] &&
+			board[i][0] != emptySpace {
+			fmt.Println("Congrats Player ", board[i][0], ", You won the game")
+			return true
+		}
+	}
+
+	// Check column
+	for i := 0; i < 3; i++ {
+		if board[0][i] == board[1][i] &&
+			board[1][i] == board[2][i] &&
+			board[0][i] != emptySpace {
+			fmt.Println("Player ", board[0][i], " won the game")
+			return true
+		}
+	}
+
+	// Check diagonal
+	if board[0][0] == board[1][1] &&
+		board[1][1] == board[2][2] &&
+		board[0][0] != emptySpace {
+		fmt.Println("Player ", board[0][0], " won the game")
+		return true
+	}
+
+	return false
+}
+
+// Check Draw - check if the game is draw
+func checkDraw() bool {
+	for i := 0; i < 3; i++ {
+		for j := 0; j < 3; j++ {
+			if board[i][j] == emptySpace {
+				return false
+			}
+		}
+	}
+	return true
+}
