27a758a4749fa69fe19d4d203344c56b6fa1bbf9
Ramananda Kairi <ramanandakairi@gmail.com>
2022-12-30 22:21:48 +0530

    fix(): fix the bug and add the feature

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 var (
-	player1    = 1
-	player2    = 2
-	emptySpace = 0
+	p1BoardValue = 1
+	p2BoardValue = 2
+	emptySpace   = 0
 )
 
 var board [3][3]int
@@ -22,30 +24,68 @@ func newBoard() {
 // printBoard - prints the board
 func printBoard() {
 	for i := 0; i < 3; i++ {
+		// var numStr []string
 		for j := 0; j < 3; j++ {
 			if board[i][j] == 2 {
-				fmt.Printf(" X |")
+				fmt.Printf("  X  | ")
 			} else if board[i][j] == 1 {
-				fmt.Printf(" O |")
+				fmt.Printf("  O  | ")
+			} else if board[i][j] == 0 {
+				fmt.Printf(" %d-%d | ", i+1, j+1)
 			} else {
-				fmt.Printf("   |")
+				panic("Invalid board")
 			}
 		}
 		fmt.Println()
-		fmt.Println("----------")
+		fmt.Println("--------------------")
 	}
 }
 
 func main() {
-	newBoard()
 	fmt.Println("Welcome to Tic Tac Toe")
 
-	currentPLayer := 1
+	var playerOneName string
+	fmt.Printf("Player 1, Please enter your name: ")
+	_, errPOne := fmt.Scanf("%s", &playerOneName)
+	if errPOne != nil {
+		fmt.Println("Invalid input, please try again")
+		return
+	}
+
+	var playerTwoName string
+	fmt.Printf("Player 2, Please enter your name: ")
+	_, errPTwo := fmt.Scanf("%s", &playerTwoName)
+	if errPTwo != nil {
+		fmt.Println("Invalid input, please try again")
+		return
+	}
+
+	// Store the Current Player Name and Board Value
+	cpMap := map[int]string{
+		p1BoardValue: playerOneName,
+		p2BoardValue: playerTwoName,
+	}
+
+	repMap := map[int]string{
+		p1BoardValue: "X",
+		p2BoardValue: "O",
+	}
+
+	fmt.Printf("Lets start with Player One - %s \n", playerOneName)
+	cpBv := p1BoardValue
+
+	fmt.Printf("Player %v will be represented by %s \n", cpMap[cpBv], repMap[cpBv])
+
+	// Get Current Player Name from the map
+
+	newBoard()
+
 	for {
+		currentPlayerName := cpMap[cpBv]
 		fmt.Println("Current Board :")
 		printBoard()
 
-		fmt.Printf("Hello Player %d, Please enter your move in row and column format (e.g. 1 2): ", currentPLayer)
+		fmt.Printf("Hello Player %s, Please enter your move in row and column format (e.g. 1 2): ", currentPlayerName)
 		var row, col int
 		_, err := fmt.Scanf("%d %d", &row, &col)
 		if err != nil {
@@ -53,35 +93,44 @@ func main() {
 			continue
 		}
 
+		// Check if the move is valid
 		if row < 1 || row > 3 || col < 1 || col > 3 {
 			fmt.Println("Invalid input, please try again")
 			continue
 		}
 
-		updateBoard(row, col, currentPLayer)
+		// Check if the place is not already occupied
+		if board[row-1][col-1] != emptySpace {
+			fmt.Println("Invalid input, please try again (place already occupied)")
+			continue
+		}
+
+		// Update the board
+		updateBoard(row, col, cpBv)
 
 		// Check if someone won the game
-		isWon := checkWon()
+		isWon := checkWon(currentPlayerName)
 		if isWon {
-			updateBoard(row, col, currentPLayer)
+			updateBoard(row, col, cpBv)
 			printBoard()
+			fmt.Println("Congrats Player ", currentPlayerName, ", You won the game")
 			break
 		}
 
 		// Check if the game is draw
 		isDraw := checkDraw()
 		if isDraw {
-			updateBoard(row, col, currentPLayer)
+			updateBoard(row, col, cpBv)
 			printBoard()
 			fmt.Println("Game is draw")
 			break
 		}
 
 		// Alternate the player
-		if currentPLayer == 1 {
-			currentPLayer = 2
+		if cpBv == p1BoardValue {
+			cpBv = p2BoardValue
 		} else {
-			currentPLayer = 1
+			cpBv = p1BoardValue
 		}
 	}
 }
@@ -91,13 +140,18 @@ func updateBoard(row, col, player int) {
 	board[row-1][col-1] = player
 }
 
-func checkWon() (isWon bool) {
+// wonMessage - prints the message when someone wins the game
+func wonMessage(player string) {
+	fmt.Println("Congrats Player ", player, ", You won the game")
+}
+
+func checkWon(player string) (isWon bool) {
 	// Check row
 	for i := 0; i < 3; i++ {
 		if board[i][0] == board[i][1] &&
 			board[i][1] == board[i][2] &&
 			board[i][0] != emptySpace {
-			fmt.Println("Congrats Player ", board[i][0], ", You won the game")
+			wonMessage(player)
 			return true
 		}
 	}
@@ -107,16 +161,23 @@ func checkWon() (isWon bool) {
 		if board[0][i] == board[1][i] &&
 			board[1][i] == board[2][i] &&
 			board[0][i] != emptySpace {
-			fmt.Println("Player ", board[0][i], " won the game")
+			wonMessage(player)
 			return true
 		}
 	}
 
-	// Check diagonal
+	// Check diagonal - 1
 	if board[0][0] == board[1][1] &&
 		board[1][1] == board[2][2] &&
 		board[0][0] != emptySpace {
-		fmt.Println("Player ", board[0][0], " won the game")
+		wonMessage(player)
+		return true
+	}
+	// Check diagonal - 2
+	if board[0][2] == board[1][1] &&
+		board[1][1] == board[2][0] &&
+		board[0][2] != emptySpace {
+		wonMessage(player)
 		return true
 	}
 
